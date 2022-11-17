9e8051896ca28fd3b8697c95a0b395667c36c3be
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-11-17 14:47:43 -0300

    atualizaçao navbar (bootstrap) e hover

--- js/navbar.js
+++
@@ -1,6 +0,0 @@
-const botaoCategoria = document.querySelector('.categoria-menu')
-const menu = document.querySelector('menu-sub')
-
-botaoCategoria.addEventListener('click', () => {
-    menu.classList.toggle('menu-escondido--ativo')
-})
